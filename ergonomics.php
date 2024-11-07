8cb05844d6ddfd5fcfacdb34cc9617c0b4095b6c
Simukelo24 <carol.gumede@gibela-rail.com>
2024-11-07 09:54:29 +0200

    Add files via upload

---
+++ ergonomics.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <title>i-Learn</title>
+    <meta name="author" content="CrossKnowledge"/>
+    <meta name="robots" content="noindex, nofollow">
+  <link rel="icon" type="image/png" href="https://alstomuniversity.eu.crossknowledge.com/images/favicon.png" />
+<script type="text/javascript">
+  window.jqueryMigrateLoggingEnabled = false;
+  window.jsErrorLoggingEnabled = false;
+  window.loggingModule = "frontoffice";
+</script>
+<script src="https://ckls-assets.eu.crossknowledge.com/assets/js/CoreLogError.js?v=9fbde388542"></script>
+    <link rel="stylesheet" href="https://ckls-assets.eu.crossknowledge.com/libs/extjs/resources/ext-theme-gray/ext-theme-gray-all.css?v=9fbde388542"/>
+    <link rel="stylesheet" href="https://ckls-assets.eu.crossknowledge.com/css/FCKEditor.css?v=9fbde388542"/>
+    <link rel="stylesheet" href="https://ckls-assets.eu.crossknowledge.com/assets/css/legacy-fo-page.css?v=9fbde388542"/>
+    <link rel="stylesheet" href="https://ckls-assets.eu.crossknowledge.com/assets/css/fonts.css?v=9fbde388542"/>
+    <link rel="stylesheet" href="https://ckls-assets.eu.crossknowledge.com/fonts/font-awesome.min.css?v=9fbde388542"/>
+    <link rel="stylesheet" href="https://ckls-assets.eu.crossknowledge.com/assets/css/fo-login-page.css?v=9fbde388542"/>
+          <link rel="stylesheet" href="/data/styles/AlstomPilot/stylesheet.css?t=166c1c8830d94e149b0e940b75cdb258" media="screen"/>
+          <link rel="stylesheet" href="/data/css/9fbde388542/AlstomPilot/1712675493/fonts/font-face.css" media="screen"/>
+    <script>
+    themeManagerUrl = "https://alstomuniversity.eu.crossknowledge.com/";
+    themeManagerWorkspace = "Base";
+    themeManagerThousandSeparator = ",";
+    themeManagerDecimalSeparator = ".";
+    themeManagerDateFormatJS = "%d/%m/%Y";
+    themeManagerLongDateFormat = "l, F j, Y";
+    themeManagerLongDateFormat = "l, F j, Y";
+    themeManagerTimeFormat = "H:i";
+    themeManagerFirstDayOfWeek = "1";
+    themeManagerAllowedExtensions = "doc,d,xls,xlsx,ppt,pptx,txt,htm,html,css,pdf,gif,jpg,jpeg,png,psd,zip,gz,tgz,rar,7z,mp3,avi,odt,odp,ods";
+    themeManagerFirstDayOfWeek = "1";
+</script>
+
+    <script src="https://cmp.osano.com/AzZdRbSORDOOzF9W/68b4f5b6-2e12-4d91-a9f9-48872d49b4fb/osano.js"></script>
+
+    <script id="jsPlaceholderDynatrace"></script>
+
+    <script src="https://alstomuniversity.eu.crossknowledge.com/site/assets/js/translations.en-GB.9fbde388542.js"></script>
+    <script src="https://ckls-assets.eu.crossknowledge.com/assets/js/extjs.js?v=9fbde388542"></script>
+    <script src="https://ckls-assets.eu.crossknowledge.com/libs/extjs/locale/ext-lang-en_GB.js?v=9fbde388542"></script>
+    <script src="https://ckls-assets.eu.crossknowledge.com/assets/js/javascript-fo-legacy-page.js?v=9fbde388542"></script>
+    <script src="https://ckls-assets.eu.crossknowledge.com/js/themeManagerHeader.js?v=9fbde388542"></script>
+
+
+<script type="text/javascript">
+
+
+    monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    monthArrayShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    dayArray = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    weekString = 'W';
+    todayString = 'Today';
+    languageCode = 'en';
+    dateFormat = "dd/mm/yyyy";
+
+</script>
+    <script>
+      window.addEventListener('DOMContentLoaded', function (event) {
+        var adobeAnalyticsProperties = '{"type":"adobe-analytics","scriptUrl":"\/\/assets.adobedtm.com\/59640f9fa510\/f174bdf3096b\/launch-c239e7226acc.min.js","additionalCSPHosts":["*.112.2o7.net","assets.adobedtm.com"]}';
+        adobeAnalyticsProperties = JSON.parse(adobeAnalyticsProperties);
+        window.InternalWebAnalyticsFactory(adobeAnalyticsProperties);
+      });
+    </script>
+    <script>
+      (function(apiKey){
+        (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
+          v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
+            o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+          y=e.createElement(n);y.async=!0;y.src='https://cdn.eu.pendo.io/agent/static/'+apiKey+'/pendo.js';
+          z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+
+        // Call this whenever information about your visitors becomes available
+        // Please use Strings, Numbers, or Bools for value types.
+        visitor_data = {
+          id: '',   // Required if user is logged in
+          user_language: '', // User preferred language
+          isNewCoreProductEnabled: 1,
+          // email:        // Recommended if using Pendo Feedback, or NPS Email
+          // full_name:    // Recommended if using Pendo Feedback
+          // role:         // Optional
+
+          // You can add any additional visitor level key-values here,
+          // as long as it's not one of the above reserved names.
+        };
+        pendo.initialize({
+          visitor: visitor_data,
+
+          account: {
+            id: 'ALSTOM', // Required if using Pendo Feedback
+            // name:         // Optional
+            // is_paying:    // Recommended if using Pendo Feedback
+            // monthly_value:// Recommended if using Pendo Feedback
+            // planLevel:    // Optional
+            // planPrice:    // Optional
+            // creationDate: // Optional
+
+            // You can add any additional account level key-values here,
+            // as long as it's not one of the above reserved names.
+          }
+        });
+      })('09cc0ea7-0d63-4915-5169-a04ae178d9f4');
+    </script>
+
+
+
+<script src="https://ckls-assets.eu.crossknowledge.com/bundles/fosjsrouting/js/router.js?v=9fbde388542"></script>
+    <script src="https://ckls-assets.eu.crossknowledge.com/assets/js/fos_js_routes.js?v=9fbde388542"></script>
+
+</head>
+<body class="loggedOut bodyLarge">
+  <div class="loginPageContainer">
+    <div class="loginHeader">
+        &nbsp;
+    </div>
+    <div class="containerLoginLarge pie">
+      <div class="headerBox">
+        <img src="https://alstomuniversity.eu.crossknowledge.com/data/logos/1.png" alt="" />
+      </div>
+
+	  		  <div class="FCK" style="padding:10px 20px 30px 20px; min-width: 440px;">
+			<p align="center" style="font-size: 35px; color: #90805F; font-family: roboto,arial,sans-serif; font-weight: bold; text-align: center; margin-top:-15px; margin-bottom:52px;">i-Learn</p>
+<table align="center" style="width: 600px;"><tbody><tr style="height: 9px;"><td style="width: 600px; height: 9px;">
+ <p align="left" style="font-size: 16px; color: #1e3246; font-family: roboto,arial,sans-serif;">Connect with the <strong><span style="color: #dc3223; font-family: roboto,arial,sans-serif; font-size: 15px;">SSO</span> </strong>button, or using your <strong>ALPS ID</strong> and password<br><span style="color: #1e3246; font-family: roboto,arial,sans-serif; font-size: 12px;">(<a href="https://alstomuniversity.eu.crossknowledge.com/interfaces/forgotten_password.php" target="_blank">Click here to generate your password</a>).</span><br>
+  </p>
+ </td>
+ </tr></tbody></table>			  <div class="clearBoth"></div>
+		  </div>
+	  
+      <div id="ajaxNotificationsHolder" style="display:none"></div>
+                  <div class="loginContainerLeft">
+             <div class="connectionButton pie pieFix"><table class="pie" style="background: url('https://alstomuniversity.eu.crossknowledge.com/data/obj_files/AUTH_DRIVER/000/SAM/L24/icon.png') no-repeat left center transparent;"><tr><td>	<a id="SAML2_button" href="javascript:forceDriver(4)" class="buttonSubmit pie">Connect with SSO</a></td></tr></table></div>			</div>
+
+		  
+      <div class="loginContainerRight">
+
+       <form class="loginForm" action="https://alstomuniversity.eu.crossknowledge.com/login_trainee.php" method="post" id="Login">
+       	<input type="hidden" name="auth_driver" value="1" />
+        <input type="hidden" name="loginFormToken" value="Yk7pkbJbtvbV3Qx_c2Bd4xdQJLW6vnKvwSnAojk7Ah0"/>
+            <div class="loginFormBox pie">
+	          <table>
+	            <tr>
+	              <th>
+	                <div><label for="fldUserName">Login:</label></div>
+	              </th>
+	              <td>
+	                <input type="text" name="login" id="fldUserName" maxlength="256"
+                           value="" tabindex="1" autocomplete="off"/>
+	              </td>
+	            </tr>
+	            <tr>
+	              <th>
+	                <label for="fldPassword">Password:</label>
+	              </th>
+	              <td>
+	                <input type="password" name="pass" id="fldPassword" maxlength="32"
+                           value="" tabindex="2" autocomplete="off"/>
+	              </td>
+	            </tr>
+	            
+	          </table>
+	        </div>
+	        <div class="loginSubmit pie pieFix">
+	          <input type="submit" name="submitButtonName" class="buttonSubmit" value="Log in" tabindex="5"/>
+	        </div>
+	      </form>
+
+      </div>
+
+      
+    </div>
+
+      <div class="loginTools pie">
+                        <div id="forgetPass" class="forgetPass">
+                  <a id="forgetPassLink" href="https://alstomuniversity.eu.crossknowledge.com/interfaces/forgotten_password.php">
+                      Forgotten password?                  </a>
+              </div>
+          
+          <div class="checkConfiguration">
+              <a href="/site/check-configuration"> Test my configuration </a>
+          </div>
+
+                        <div>
+                  <a href="#" onclick="Osano.cm.showDrawer('osano-cm-dom-info-dialog-open')">Cookie preferences</a>
+              </div>
+          
+      </div>
+  </div>
+
+  <form id="forceAuthDriver" method="post" action="https://alstomuniversity.eu.crossknowledge.com/login_trainee.php">
+      <input type="hidden" name="auth_driver" value=""/>
+  </form>
+
+  <script type="text/javascript">
+      function forceDriver(driverId) {
+          $('forceAuthDriver').auth_driver.value = driverId;
+          $('forceAuthDriver').submit();
+      }
+  </script>
+	<!--[if IE 6]>
+	<script type="text/javascript" src="https://alstomuniversity.eu.crossknowledge.com/js/interfaces/login.js"></script>
+	<![endif]-->
+  
+
+
+
+
+
+
+
+
+</body>
+
+</html>
